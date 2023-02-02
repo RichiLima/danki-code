48f529a41e8643d0b806c578b33e5535c60ec4f9
Richard Lima <112436267+RichiLima@users.noreply.github.com>
2023-02-02 16:55:07 -0300

    atualizão das aulas da danki code

---
+++ CURSO-FRONT-END/Script.js
@@ -0,0 +1,30 @@
+
+window.onload = function(){
+
+    var nome = 'Richard';
+    var nome = 'grazi';
+
+    if(nome == 'Richard'){
+        alert('Condição Verdadeira');
+    }else if(nome == 'grazi'){
+        alert('Condição Verdadeira');
+    }else{
+        alert('Condição Falsa');
+    }
+}
+
+window.onload = function(){
+
+    var idade = 2023-2001;
+
+    if(idade >= 26){
+        console.log('Eu tenho mais 26 anos de idade')
+    }else if(idade < 10){
+        console.log('Eu tenho menos de 11 anos de idade')
+    }else if(idade < 5){
+        console.log('Eu tenho menos de 6 anos de idade')
+    }else {
+        console.log('Nenhuma das condições são verdadeiras')
+    }
+
+}
